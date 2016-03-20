900193d002bf331609d8b18fba6506d78422e280
angcyo <angcyo@126.com>
2016-03-20 23:16:16 +0800

    retrofit test

--- src/com/angcyo/servlet/ApiServlet.java
+++ src/com/angcyo/servlet/ApiServlet.java
@@ -8,10 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.Collection;
 import java.util.Enumeration;
 import java.util.Map;
 
@@ -23,17 +22,37 @@ public class ApiServlet extends HttpServlet {
     final static PrintStream p = System.out;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        printRequestInfo(request);
+//        printRequestInfo(request);
 
-        ResponseUtil.write(response, "doPost " + TextUtils.getDataTime());
+        String parameter = "no";
+        if (isGet(request)) {
+            parameter = parseGetParamJson(request);
+        } else if (isPost(request)) {
+            parameter = parsePostParamJson(request);
+        }
+
+        if (TextUtils.isEmpty(parameter)) {
+            parameter = "no";
+        }
+
+        String src =
+                "{" +
+                        "\"rid\":12," +
+                        "\"name_char\":\"马化腾\"," +
+                        "\"name_varchar\":\"埃里巴巴\"," +
+                        "\"param\":" + parameter + "," +
+                        "\"name_float\":0," +
+                        "\"name_int\":0," +
+                        "\"name_tinyint\":0," +
+                        "\"name_time\":1478833860000" +
+                        "}";
+
+        ResponseUtil.write(response, src);
     }
 
     private void printRequestInfo(HttpServletRequest request) throws IOException, ServletException {
         p.println("getContextPath-->" + request.getContextPath());
-        p.println("getContentLength-->" + request.getContentLength());
-        p.println("getContentType-->" + request.getContentType());
 
-        p.println("getMethod-->" + request.getMethod());
         p.println("getAuthType-->" + request.getAuthType());
         p.println("getServletPath-->" + request.getServletPath());
 
@@ -41,32 +60,122 @@ public class ApiServlet extends HttpServlet {
         p.println("getRequestURI-->" + request.getRequestURI());
         p.println("getRequestedSessionId-->" + request.getRequestedSessionId());
         p.println("getRemoteUser-->" + request.getRemoteUser());
-        p.println("getRequestURL-->" + request.getRequestURL().toString());
         p.println("getLocalName-->" + request.getLocalName());
         p.println("getRemoteAddr-->" + request.getRemoteAddr());
         p.println("getServerName-->" + request.getServerName());
         p.println("getRemoteHost-->" + request.getRemoteHost());
         p.println("getServerPort-->" + request.getServerPort());
-        p.println("getScheme-->" + request.getScheme());
         p.println("getLocalAddr-->" + request.getLocalAddr());
 
         Enumeration<String> headerNames = request.getHeaderNames();
 
-        p.println("contextPath-->" + "-");
+        while (headerNames.hasMoreElements()) {//以此取出头信息
+            String headerName = headerNames.nextElement();
+            String headerValue = request.getHeader(headerName);//取出头信息内容
 
-        Collection<Part> parts = request.getParts();
-        for (Part part : parts) {
-            p.println("getParts-->" + part.getName());
+            p.println("getHeaderNames-->" + "header->" + headerName + " value->" + headerValue);
         }
 
-        Map<String, String[]> parameterMap = request.getParameterMap();
-        for (Map.Entry<String, String[]> map : parameterMap.entrySet()) {
-            p.println("getParameterMap-->" + "key->" + map.getKey() + " value->" + map.getValue());
+//        p.println("contextPath-->" + "-");
+
+//        Collection<Part> parts = request.getParts();
+//        for (Part part : parts) {
+//            p.println("getParts-->" + part.getName());
+//        }
+
+
+        p.println("------------------------------------------------------------------------------");
+        p.println("getRequestURL-->" + request.getRequestURL().toString());
+        p.println("getContentLength-->" + request.getContentLength());
+        p.println("getMethod-->" + request.getMethod());
+        p.println("getScheme-->" + request.getScheme());
+        p.println("getContentType-->" + request.getContentType());
+        if (isGet(request)) {
+            Map<String, String[]> parameterMap = request.getParameterMap();
+            for (Map.Entry<String, String[]> map : parameterMap.entrySet()) {
+                p.print("GET-getParameterMap-->" + "key->" + map.getKey() + " value->");
+                for (String v : map.getValue()) {
+                    p.print(v + " ");
+                }
+                p.println();
+            }
+        } else if (isPost(request)) {
+            BufferedReader reader = request.getReader();
+            StringBuilder builder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+
+            p.println("POST-getParameterMap-->" + builder.toString());
         }
 
     }
 
+    protected String parseGetParam(HttpServletRequest request) {
+        StringBuilder builder = new StringBuilder();
+
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        for (Map.Entry<String, String[]> map : parameterMap.entrySet()) {
+
+        }
+
+        return builder.toString();
+    }
+
+    protected String parsePostParam(HttpServletRequest request) {
+        StringBuilder builder = new StringBuilder();
+
+        return builder.toString();
+    }
+
+    /**
+     * 获取URL中的所有参数信息
+     */
+    protected String parseGetParamJson(HttpServletRequest request) {
+        StringBuilder builder = new StringBuilder("{");
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        for (Map.Entry<String, String[]> map : parameterMap.entrySet()) {
+            builder.append("\"").append(map.getKey()).append("\"").append(":").append(map.getValue()[0]).append(",");
+        }
+
+        int length = builder.length();
+        if (length > 1) {
+            builder.deleteCharAt(length - 1);
+        }
+
+        builder.append("}");
+        return builder.toString();
+    }
+
+    /**
+     * 获取body内,所有的参数信息
+     */
+    protected String parsePostParamJson(HttpServletRequest request) {
+        StringBuilder builder = new StringBuilder();
+        try {
+            BufferedReader reader = request.getReader();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+        } catch (Exception e) {
+
+        }
+
+        return builder.toString();
+    }
+
+    protected boolean isGet(HttpServletRequest request) {
+        return request.getMethod().equalsIgnoreCase("GET");
+    }
+
+    protected boolean isPost(HttpServletRequest request) {
+        return request.getMethod().equalsIgnoreCase("POST");
+    }
+
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
+        doPost(request, response);
     }
 }
